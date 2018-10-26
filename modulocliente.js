d7992f91dffdd3d40c97c9502ab976f400d64324
isaiassm <isaias1113@outlook.com>
2018-10-25 22:07:19 -0300

    inicio de node e entendo a estrutura de modulos que é trabalhado dentro do node

---
+++ modulocliente.js
@@ -0,0 +1,12 @@
+const moduloA = require('./modulo')
+const moduloB = require('./modulob')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
+
